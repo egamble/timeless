acb1aa9859381d50b3d4ebc4a0238d16a25cd96b
Evan Gamble <egamble@factual.com>
2014-06-30 02:17:47 -0700

    making instaparse grammer for TL

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject egamble/timeless "0.1.0"
   :description "Timeless compiler"
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.memoize "0.5.6"]
                  [factual/fnparse "2.3.0"]
-                 [egamble/let-else "1.0.6"]]
+                 [egamble/let-else "1.0.7"]
+                 [instaparse "1.3.2"]]
   :min-lein-version "2.0.0"
   :url "https://github.com/egamble/timeless")
