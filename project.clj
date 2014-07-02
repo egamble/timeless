fdff014717069b2f937e6f0d25f2e1c28af9ad54
Evan Gamble <egamble@factual.com>
2014-07-01 23:44:18 -0700

    unary member-of operation

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
   :description "Timeless compiler"
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.memoize "0.5.6"]
-                 [factual/fnparse "2.3.0"]
                  [egamble/let-else "1.0.7"]
                  [instaparse "1.3.2"]]
   :min-lein-version "2.0.0"
