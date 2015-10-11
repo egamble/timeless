e82a899462d3c642348ef7312e27ba4fca262e23
Evan Gamble <solar.fuel@gmail.com>
2015-10-11 02:33:59 -0700

    remove dep on let-else and instaparse

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject egamble/timeless "0.1.0"
   :description "Timeless compiler"
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [egamble/let-else "1.0.7"]
-                 [instaparse "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :min-lein-version "2.0.0"
   :url "https://github.com/egamble/timeless")
