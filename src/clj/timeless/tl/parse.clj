9356f760f663ff030d5583c2af0ce6614c4b956d
Evan Gamble <solar.fuel@gmail.com>
2020-06-21 15:52:01 +0200

    changes

--- src/clj/timeless/tl/parse.clj
+++ src/clj/timeless/tl/parse.clj
@@ -5,16 +5,54 @@
             [clojure.string :as str]))
 
 
-(def as-and-bs
+;;; Don't prefixize comparison expressions, including comparison chains and embedded assertions. Just surround them with a "comparison" type. In post-processing, expand embedded assertions if they're not within (original) parens.
+;;; Don't insert "missing" in front of the source. Just check in post-processing that the top level expression is a section of a guard.
+;;; In post-processing, extract the assertions from the top leval guard section and the prefixized guard expressions within it.
+;;; Use insta/add-line-and-column-info-to-metadata so line/column info is available to generate errors when post-processing.
+
+;;; When parsing a chain of operators (other than all comparisons), an error is produced if (a) the operators don't have a relative precedence, or if (b) the operators have equal precedence but one is left assoc and another is right assoc. Equal precedence, with one left or right and another simply assoc, should be okay, but how can that be written as a parse rule?
+
+(def parser
   (insta/parser
-    "S = (AB | WS)*
-     AB = A B
-     A = 'a'+
-     B = 'b'+
-     WS = '\n'"))
+    "
+<S> = exp
+<exp> = non-appl-exp | application
+<non-appl-exp> = guard-op | seq | tup | str | name
+application = non-appl-exp non-appl-exp+
+
+
+element = (epsilon | exp)
+
+(* If a seq ends with a comma, an empty element is produced, for a seq section. *)
+(* There is no way to make a seq section with a single empty element. *)
+
+seq = (seq-left seq-right) | (seq-left (element comma)+ element seq-right)
+
+<seq-left> = ws <'['> ws
+<seq-right> = ws <']'> ws
+
+
+tup = (tup-left (element comma)+ element tup-right)
+
+<tup-left> = ws <'('> ws
+<tup-right> = ws <')'> ws
+
+
+str = ws #'\".*\"' ws
+guard-op = ws <'|'> ws
+name = ws #'[a-zA-Z_]\\w*' ws
+
+<comma> = ws <','> ws
+
+<ws> = (ws-only | comment)*
+<ws-only> = <#'\\s+'>
+<comment> = <#'#.*\n'>"))
+
 
 ;; Returns: <assertions>
 (defn parse [path declarations source]
-  (let [pr-matrix (build-pr-matrix declarations)]
-    (println (as-and-bs "aaaaabbb\naaabb"))
-    nil))
+  (let [pr-matrix (build-pr-matrix declarations)
+        parsed (parser source)]
+    (if (insta/failure? parsed)
+      (println parsed)
+      parsed)))
