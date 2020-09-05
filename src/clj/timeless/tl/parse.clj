30ef1e237f70a7a43924ac469f407613fbd2ab65
Evan Gamble <solar.fuel@gmail.com>
2020-09-05 22:01:55 +0200

    changes

--- src/clj/timeless/tl/parse.clj
+++ src/clj/timeless/tl/parse.clj
@@ -5,9 +5,16 @@
             [clojure.string :as str]))
 
 
-;;; Don't prefixize comparison expressions, including comparison chains and embedded assertions. Just surround them with a "comparison" type. In post-processing, expand embedded assertions if they're not within (original) parens.
-;;; Don't insert "missing" in front of the source. Just check in post-processing that the top level expression is a section of a guard.
-;;; In post-processing, extract the assertions from the top leval guard section and the prefixized guard expressions within it.
+;;; All operations except guard operations are prefixized during parsing, including comparison chains, embedded assertions, and arrow operations. The operations may be reformed and expanded in post-processing. Comparisons must be associative (probably right-associative) to allow comparison chains, even if they are formally non-associative.
+
+;;; Guard operations are not prefixized because they are left-associative, which makes checking for partial comparisons on their left sides difficult if they were prefixized.
+
+;;; Leave grouping parens exposed after parsing, i.e. all parens except those that denote sections, prefixized operators, and tuples. The exposed parens allow correct post-processing of comparison chains, and abbreviated arrow and guard operations.
+
+;;; Don't insert "missing" in front of the source. Just check in post-processing that the top level expression is a guard with a guard operator on the left.
+
+;;; In post-processing, extract the assertions from the top level guard expressions.
+
 ;;; Use insta/add-line-and-column-info-to-metadata so line/column info is available to generate errors when post-processing.
 
 ;;; When parsing a chain of operators (other than all comparisons), an error is produced if (a) the operators don't have a relative precedence, or if (b) the operators have equal precedence but one is left assoc and another is right assoc. Equal precedence, with one left or right and another simply assoc, should be okay, but how can that be written as a parse rule?
