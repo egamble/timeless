d11d281d8ab0df3fb753b9d085b85aa9dce4084c
Evan Gamble <solar.fuel@gmail.com>
2020-11-23 18:32:15 +0100

    changes

--- src/clj/timeless/tl/ops.clj
+++ src/clj/timeless/tl/ops.clj
@@ -14,57 +14,88 @@
     ("#opl" 1 "|")
     ("#opr" 0 "->" "→")))
 
+(defn third [s] (nth s 2))
+
 (defn build-op-declarations [declarations]
-  (map (fn [declaration]
-         (let [declaration (rest declaration)]
-           (cons (first declaration)
-                 (cons (read-string (second declaration))
-                       (rest (rest declaration))))))
+  (map (fn [[_ assoc priority-str & names]]
+         `(~assoc ; associativity: "#op" or "#opr" or "#opl"
+           ~(read-string priority-str) ; numeric priority
+           ~@names ; names of the operators
+           ))
        (filter #(#{"#op" "#opr" "#opl"} (second %))
                declarations)))
 
-(defn build-associative-grammar-for-each-op [declaration]
-  (let [pr (second declaration)]
-    (when (not (#{0 1} pr))
-      (case (first declaration)
-        ("#op" "#opr")
-        (str
-         (format "<left-%d> = left-paren gt-%d op-%d right-paren\n" pr pr pr)
-         (format "<right-%d> = left-paren op-%d gte-%d right-paren\n" pr pr pr)
-         (format "operation-%d = gt-%d op-%d gte-%d\n" pr pr pr pr))
+(defn stringify-utf8 [s]
+  (->> (.getBytes s "UTF-8")
+       (map (partial format "%02x"))
+       (apply str)))
 
-        "#opl"
-        (str
-         (format "<left-%d> = left-paren gte-%d op-%d right-paren\n" pr pr pr)
-         (format "<right-%d> = left-paren op-%d gt-%d right-paren\n" pr pr pr)
-         (format "operation-%d = gte-%d op-%d gt-%d\n" pr pr pr pr))))))
+(defn make-op-terminal [name]
+  (str (if (re-matches #"[a-zA-Z]\w*" name)
+         name
+         (str "_" (stringify-utf8 name)))
+       "-op"))
+
+(defn build-associative-grammar-for-each-op [[assoc pr & names]] ; associativity, numeric priority and op names
+  (when (not (#{0 1} pr))
+    (let [op-terminals (map make-op-terminal names)]
+      (str
+       (case assoc
+         ("#op" "#opr")
+         (str
+          (format "<left-%d> = left-paren gt-%d op-%d right-paren\n" pr pr pr)
+          (format "<right-%d> = left-paren op-%d gte-%d right-paren\n" pr pr pr)
+          (format "operation-%d = gt-%d op-%d gte-%d\n" pr pr pr pr))
+
+         "#opl"
+         (str
+          (format "<left-%d> = left-paren gte-%d op-%d right-paren\n" pr pr pr)
+          (format "<right-%d> = left-paren op-%d gt-%d right-paren\n" pr pr pr)
+          (format "operation-%d = gte-%d op-%d gt-%d\n" pr pr pr pr)))
+       
+       (format "<op-%d> = %s\n"
+               pr
+               (->> (interleave op-terminals
+                                 (repeat " | "))
+                    butlast
+                    (apply str)))
+
+       (->> (map (fn [op-terminal name]
+                         (format "%s = ws <'%s'> ws\n" op-terminal name))
+                 op-terminals names)
+            (apply str))))))
 
 
-(defn build-grammar-for-each-op-but-last [[declaration next-declaration]]
-  (let [pr (second declaration)
-        next-pr (second next-declaration)]
-    (str
-     (format "<gte-%d> = exp | _gte-%d\n" pr pr)
-     (format "<gt-%d> = exp | _gt-%d\n" pr pr)
-     (format "<_gte-%d> = operation-%d | _gt-%d\n" pr pr pr)
-     (format "<_gt-%d> = _gte-%d\n" pr next-pr))))
+(defn build-grammar-for-each-op-but-last [[[_ pr & _] [_ next-pr & _]]]
+  ;; pr is the numeric priority of the current op declaration and
+  ;; next-pr is the priority of the next op declaration
+  (str
+   (format "<gte-%d> = exp | _gte-%d\n" pr pr)
+   (format "<gt-%d> = exp | _gt-%d\n" pr pr)
+   (format "<_gte-%d> = operation-%d | _gt-%d\n" pr pr pr)
+   (format "<_gt-%d> = _gte-%d\n" pr next-pr)))
 
-(defn build-grammar-for-last-op [declaration]
-  (let [pr (second declaration)]
-    (str
-     (format "<gte-%d> = exp | _gte-%d\n" pr pr)
-     (format "<gt-%d> = exp\n" pr)
-     (format "<_gte-%d> = operation-%d" pr pr))))
+(defn build-grammar-for-last-op [[_ pr & _]]
+  ;; pr is the numeric priority
+  (str
+   (format "<gte-%d> = exp | _gte-%d\n" pr pr)
+   (format "<gt-%d> = exp\n" pr)
+   (format "<_gte-%d> = operation-%d" pr pr)))
 
 
 (defn p [x] (println x) x)
 
+
+;; TODO
+(defn combine-same-priorities [op-declarations] op-declarations)
+
 ;; TODO: check syntax of declarations, including that operators of the same precedence must have the same associativity, and that declaring a predefined name is an error.
 
 (defn build-operator-grammar [declarations]
-  (let [op-declarations (sort-by second
-                                 (concat (build-op-declarations declarations)
-                                         predefined-op-declarations))]
+  (let [op-declarations (->> (concat (build-op-declarations declarations)
+                                     predefined-op-declarations)
+                             (sort-by second) ; sort by the numeric priority
+                             combine-same-priorities)]
     (p (apply str
               (concat (map build-associative-grammar-for-each-op op-declarations)
                       (map build-grammar-for-each-op-but-last
